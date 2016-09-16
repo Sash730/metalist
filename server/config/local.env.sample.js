bdbddf02912e226af7f2907209254c31b1a365c7
Ruslan Polutsygan <ruslan.polutsygan@gmail.com>
2016-09-16 18:33:03 +0300

    liqpay integration more or less: generating signed link for payment, theoretical callback url

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -20,4 +20,8 @@ module.exports = {
 
     MAILER_AUTH_USER: 'example@gmail.com',
     MAILER_AUTH_PASS: 'p@ssword',
+
+    LIQPAY_PUBLIC_KEY: '***REMOVED***',
+    LIQPAY_PRIVATE_KEY: '***REMOVED***',
+    LIQPAY_SANDBOX_MODE: 1, // 0 or 1
 };
