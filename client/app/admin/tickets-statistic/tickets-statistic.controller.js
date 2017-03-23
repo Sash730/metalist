ece9ef25b610961783f16454a0063317dcd4b051
Sash730 <i_730@ukr.net>
2017-03-23 08:40:20 +0200

    prepared tickets for statistic

--- client/app/admin/tickets-statistic/tickets-statistic.controller.js
+++ client/app/admin/tickets-statistic/tickets-statistic.controller.js
@@ -28,22 +28,54 @@
     }
 
     makeStatisticsForDays(statistic) {
-      let responses = [];
-      statistic.forEach(day => {
-        let key = day.date,
-            response = responses[key];
-
-        if (!response) {
-          response = responses[key] = [];
+      let obj = statistic.reduce((acc, stat) => {
+        let p = stat.date;
+        if (!acc[0].hasOwnProperty(p)) acc[0][p] = [];
+        acc[0][p].push(stat.amount);
+        return acc;
+      },[{}])
+        .reduce((acc, v) => {
+          Object.keys(v).forEach(function(k){acc.push({day:k, amount:v[k]})});
+          return acc;
+        },[]);
+      let obj1 = obj.map(day => {
+        let amount = this.getUnicAmountCount(day.amount),
+          total = this.getTotal(amount);
+        return {
+          date: day.day,
+          amount: amount,
+          total: total
         }
-        response.push( day.amount );
       });
-      responses.map(day  => {
-
-      });
-
-      console.log(responses);
+      console.log('obj', obj1);
+      return obj1;
     }
+
+    getUnicAmountCount(amount) {
+      let responses = [];
+      amount.forEach((am) => {
+        if (!responses.filter(resp => resp.amount === am).length) {
+          responses.push({amount: am , count: 1});
+        } else {
+          responses.forEach(resp => {
+            if(resp.amount === am) resp.count++;
+          })
+        }
+
+      });
+      console.log('responses', responses);
+      return responses;
+    }
+
+    getTotal(amount) {
+      let total = 0;
+      console.log('am', amount);
+      amount.forEach((am) => {
+        return total += am.amount * am.count;
+      });
+      return total;
+    }
+
   }
 
   angular.module('metalistTicketsApp.admin')
